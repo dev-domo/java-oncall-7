8d3b4f4126358f8bec16779ea2bd70aef81367c7
pakaheo <k70923@konkuk.ac.kr>
2024-12-08 11:24:41 +0900

    feat : 서비스 출력 기능 구현

--- src/main/java/oncall/constants/OutputMessage.java
+++ src/main/java/oncall/constants/OutputMessage.java
@@ -1,7 +1,9 @@
 package oncall.constants;
 
 public enum OutputMessage {
-    EXAMPLE("");
+    INPUT_MONTH_AND_DAY("비상 근무를 배정할 월과 시작 요일을 입력하세요> "),
+    INPUT_WORKERS_BY_WEEKDAYS_TURN("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "),
+    INPUT_WORKERS_BY_HOLIDAY_TURN("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
 
     private final String message;
 
--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,4 +1,22 @@
 package oncall.view;
 
+import oncall.constants.OutputMessage;
+import oncall.domain.Schedule;
+
 public class OutputView {
+    public void promptForInputMonthAndDayOfWeek() {
+        System.out.print(OutputMessage.INPUT_MONTH_AND_DAY.getMessage());
+    }
+
+    public void promptForInputWeekdaysWorkers() {
+        System.out.print(OutputMessage.INPUT_WORKERS_BY_WEEKDAYS_TURN.getMessage());
+    }
+
+    public void promptForInputHolidayWorkers() {
+        System.out.println(OutputMessage.INPUT_WORKERS_BY_HOLIDAY_TURN.getMessage());
+    }
+
+    public void showEmergencyWorkSchedule(Schedule schedule) {
+        System.out.println(schedule.toString());
+    }
 }
