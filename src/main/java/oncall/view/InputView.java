e91d0b8c8e2caf6c5f22f9f72252452fd846c4c0
pakaheo <k70923@konkuk.ac.kr>
2024-12-08 11:24:56 +0900

    feat : 서비스 사용자 입력 기능 구현

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,4 +1,11 @@
 package oncall.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public String answer() {
+        String input = Console.readLine();
+        Console.close();
+        return input;
+    }
 }
