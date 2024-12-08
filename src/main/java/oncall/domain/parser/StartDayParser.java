94372880c4fcc755de388ad50812fbc55737bd68
pakaheo <k70923@konkuk.ac.kr>
2024-12-08 13:12:38 +0900

    refactor : 월이 1보다 작거나 12보다 크면 예외

--- src/main/java/oncall/domain/parser/StartDayParser.java
+++ src/main/java/oncall/domain/parser/StartDayParser.java
@@ -2,20 +2,28 @@ package oncall.domain.parser;
 
 import java.time.LocalDate;
 import oncall.constants.Days;
+import oncall.constants.ExceptionMessage;
 import oncall.util.IntegerUtil;
 import oncall.util.StringUtil;
 
 public class StartDayParser implements Parser {
-    private static final int MONTH_PLACE = 0;
-    private static final int DAY_PLACE = 1;
+    private static final int JANUARY = 1;
+    private static final int DECEMBER = 12;
     private static final int YEAR = 2024;
 
     @Override
     public LocalDate parse(String input) {
         String[] splitted = StringUtil.toArray(input);
-        int month = IntegerUtil.toInt(splitted[MONTH_PLACE]);
-        String day = splitted[DAY_PLACE];
+        String day = splitted[1];
 
-        return Days.convertToDayOfWeekValue(LocalDate.of(YEAR, month, 1), day);
+        return Days.convertToDayOfWeekValue(LocalDate.of(YEAR, findMonth(splitted[0]), 1), day);
+    }
+
+    private int findMonth(String textMonth) {
+        int month = IntegerUtil.toInt(textMonth);
+        if (month < JANUARY || month > DECEMBER) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT.getMessage());
+        }
+        return month;
     }
 }
